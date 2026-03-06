9af4e144c2419bb8b19a4899dcd2310bb083b503
pullsina <sovya22@gmail.com>
2026-03-06 14:55:42 +0100

    Added validation to SubmitAnswerRequestDto

--- CyberQuiz.Shared/DTOs/Quiz/SubmitAnswerRequestDto.cs
+++ CyberQuiz.Shared/DTOs/Quiz/SubmitAnswerRequestDto.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CyberQuiz.Shared.DTOs.Quiz
 {
     public class SubmitAnswerRequestDto
     {
+        [Range(1, int.MaxValue, ErrorMessage = "SubCategoryId must be greater than 0.")]
         public int SubCategoryId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "QuestionId must be greater than 0.")]
         public int QuestionId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "SelectedAnswerOptionId must be greater than 0.")]
         public int SelectedAnswerOptionId { get; set; }
     }
 }

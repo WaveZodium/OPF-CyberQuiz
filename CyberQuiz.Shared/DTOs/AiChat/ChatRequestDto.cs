4f99e8111d9622273611463c6602e841c62e5ff8
pullsina <sovya22@gmail.com>
2026-03-12 09:17:41 +0100

    Added comment to ChatRequestDto

--- CyberQuiz.Shared/DTOs/AiChat/ChatRequestDto.cs
+++ CyberQuiz.Shared/DTOs/AiChat/ChatRequestDto.cs
@@ -5,8 +5,17 @@ using System.Text;
 namespace CyberQuiz.Shared.DTOs.AiChat
 {
     public class ChatRequestDto
+    //a package data wich UI sends to API when user clicks "Send" button in chat interface
     {
+        //Message is the new user message that we want to send to AI
         public string Message { get; set; } = string.Empty;
-        public List<ChatMessageDto> History { get; set; } = [];
+
+        //History is the list of all previous messages in the conversation,
+        //including both user and AI messages
+        public List<ChatMessageDto> History { get; set; } = [];//[] - means that the List is initialized as an empty list,
+                                                               //so we can add messages to it without worrying about null reference exceptions.
+        // Beccause History is the ChatMessageDto list,
+        // it can contain both user and assistant messages,
+        // and the Role property in each ChatMessageDto will indicate who sent that message.
     }
 }
